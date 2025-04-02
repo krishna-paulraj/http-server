1e42a649f076e6b389d97ef2f4f80e90c17ef00c
krishna-paulraj <krishnapaulraj2004@gmail.com>
2025-04-02 22:48:42 +0530

    request 404

--- app/main.ts
+++ app/main.ts
@@ -4,7 +4,14 @@ import * as net from "net";
 console.log("Logs from your program will appear here!");
 
 const server = net.createServer((socket) => {
-  socket.write(Buffer.from("HTTP/1.1 200 OK\r\n\r\n"));
+  socket.on("data", (data) => {
+    const param: String = data.toString();
+    if (false) {
+      socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+    } else {
+      socket.write(Buffer.from("HTTP/1.1 200 OK\r\n\r\n"));
+    }
+  });
 });
 
 server.listen(4221, "localhost");
