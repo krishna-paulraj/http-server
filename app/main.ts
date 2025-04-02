d66a95faf892f5fcc0c8bd86c8a9f68c7ab750bd
krishna-paulraj <krishnapaulraj2004@gmail.com>
2025-04-02 22:27:14 +0530

    http status 200

--- app/main.ts
+++ app/main.ts
@@ -4,9 +4,7 @@ import * as net from "net";
 console.log("Logs from your program will appear here!");
 
 const server = net.createServer((socket) => {
-  socket.on("close", () => {
-    socket.end();
-  });
+  socket.write(Buffer.from("HTTP/1.1 200 OK\r\n\r\n"));
 });
 
 server.listen(4221, "localhost");
