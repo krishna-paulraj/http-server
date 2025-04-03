6f53a6359764f328d8a48c989170cea0e74c6dd8
krishna-paulraj <krishnapaulraj2004@gmail.com>
2025-04-03 14:55:24 +0530

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,5 @@
 import * as net from "net";
+import fs from "fs";
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
@@ -13,6 +14,7 @@ const server = net.createServer((socket) => {
     const request = data.toString();
     const path = request.split(" ")[1];
     const param = path.split("/")[1];
+    const args = process.argv;
 
     switch (param) {
       case "": {
@@ -31,6 +33,23 @@ const server = net.createServer((socket) => {
         console.log(userAgent);
         const response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`;
         sendResponse(response);
+        break;
+      }
+      case "files": {
+        const dir = args.slice(3)[0];
+        const fileName = path.split("/")[2];
+        const filePath = dir + fileName;
+
+        if (fs.existsSync(filePath)) {
+          const fileContent = fs.readFileSync(filePath).toString();
+          console.log(fileContent.length);
+          sendResponse(
+            `HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${fileContent.length}\r\n\r\n${fileContent}`,
+          );
+        } else {
+          sendResponse("HTTP/1.1 404 Not Found\r\n\r\n");
+        }
+
         break;
       }
       default: {
