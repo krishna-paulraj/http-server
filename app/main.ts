7ab253a35d5c091a3aa830140755c293b4c4ced5
krishna-paulraj <krishnapaulraj2004@gmail.com>
2025-04-03 15:26:17 +0530

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -12,6 +12,7 @@ const server = net.createServer((socket) => {
 
   socket.on("data", (data) => {
     const request = data.toString();
+    const method = data.toString().split(" ")[0];
     const path = request.split(" ")[1];
     const param = path.split("/")[1];
     const args = process.argv;
@@ -30,7 +31,6 @@ const server = net.createServer((socket) => {
       }
       case "user-agent": {
         const userAgent = request.split("User-Agent: ")[1].split("\r\n")[0];
-        console.log(userAgent);
         const response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`;
         sendResponse(response);
         break;
@@ -40,16 +40,29 @@ const server = net.createServer((socket) => {
         const fileName = path.split("/")[2];
         const filePath = dir + fileName;
 
-        if (fs.existsSync(filePath)) {
-          const fileContent = fs.readFileSync(filePath).toString();
-          console.log(fileContent.length);
-          sendResponse(
-            `HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${fileContent.length}\r\n\r\n${fileContent}`,
-          );
-        } else {
-          sendResponse("HTTP/1.1 404 Not Found\r\n\r\n");
-        }
+        switch (method) {
+          case "GET": {
+            if (fs.existsSync(filePath)) {
+              const fileContent = fs.readFileSync(filePath).toString();
+              sendResponse(
+                `HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${fileContent.length}\r\n\r\n${fileContent}`,
+              );
+            } else {
+              sendResponse("HTTP/1.1 404 Not Found\r\n\r\n");
+            }
+            break;
+          }
 
+          case "POST": {
+            const content = request.split("\r\n\r\n")[1];
+            console.log(content);
+            fs.writeFileSync(filePath, content);
+            if (fs.existsSync(filePath)) {
+              sendResponse("HTTP/1.1 201 Created\r\n\r\n");
+            }
+            break;
+          }
+        }
         break;
       }
       default: {
