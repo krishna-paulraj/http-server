7eff9602bb44d3320d5fe363a346effdd5bd1f06
krishna-paulraj <krishnapaulraj2004@gmail.com>
2025-04-03 09:28:46 +0530

    fix

--- app/main.ts
+++ app/main.ts
@@ -5,11 +5,12 @@ console.log("Logs from your program will appear here!");
 
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
-    const param: String = data.toString();
-    if (true) {
-      socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
-    } else {
+    const request = data.toString();
+    const param = request.split(" ")[1];
+    if (param == "/") {
       socket.write(Buffer.from("HTTP/1.1 200 OK\r\n\r\n"));
+    } else {
+      socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
     }
 
     socket.end();
